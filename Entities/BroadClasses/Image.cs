2d35da27035c824666fde33c85032086951833f5
Labros Papadimitriou <papadimitriou.lb@gmail.com>
2022-02-24 01:07:35 +0200

    Added Classes: Event,Article,ArticleCategory,Image. Added Seeding For Those Classes. Extra Properties and Relations between them are Commented for now!

--- Entities/BroadClasses/Image.cs
+++ Entities/BroadClasses/Image.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Entities.BroadClasses
 {
     public class Image
     {
+        public int ImageId { get; set; }
+
+        [Required(ErrorMessage = "Image URL is needed!")]
+        [Display(Name = "Image URL")]
+        public string Url { get; set; }
     }
 }
