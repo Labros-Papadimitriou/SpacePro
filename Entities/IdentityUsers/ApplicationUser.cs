29f800244a1fd08c479f874745f6d417bb6f07de
Κωστής Ζαχαριουδάκης <zacharioudakis.k@outlook.com>
2022-02-24 14:08:19 +0200

    firstname in user

--- Entities/IdentityUsers/ApplicationUser.cs
+++ Entities/IdentityUsers/ApplicationUser.cs
@@ -11,6 +11,7 @@ namespace Entities.IdentityUsers
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
