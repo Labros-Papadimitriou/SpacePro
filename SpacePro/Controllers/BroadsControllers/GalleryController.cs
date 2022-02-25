7074a63aaf83bef5466916fb52047f57130883bf
Labros Papadimitriou <papadimitriou.lb@gmail.com>
2022-02-25 22:55:18 +0200

    Additional Views and href added

---
+++ SpacePro/Controllers/BroadsControllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpacePro.Controllers.BroadsControllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult ShowGallery()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpacePro/Controllers/BroadsControllers/SocialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpacePro.Controllers.BroadsControllers
+{
+    public class SocialController : Controller
+    {
+        // GET: Social
+        public ActionResult ShowSocial()
+        {
+            return View();
+        }
+    }
+}
