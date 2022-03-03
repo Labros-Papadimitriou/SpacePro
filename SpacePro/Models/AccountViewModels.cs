13e8b81a26ed71631fb47bcba81992be457a486b
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-03 17:41:30 +0200

    User Login Changed Email to UserName

--- SpacePro/Models/AccountViewModels.cs
+++ SpacePro/Models/AccountViewModels.cs
@@ -49,9 +49,9 @@ namespace SpacePro.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+      
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
