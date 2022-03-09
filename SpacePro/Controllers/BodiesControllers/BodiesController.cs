a838d7581b26cc9b2f82215536ed15b928ca3807
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-09 18:44:15 +0200

    Dependency injection of UnitOfWork and Helper Classes

--- SpacePro/Controllers/BodiesControllers/BodiesController.cs
+++ SpacePro/Controllers/BodiesControllers/BodiesController.cs
@@ -1,18 +1,20 @@
 ﻿using MyDatabase;
+using Persistance_UnitOfWork;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SpacePro.Controllers.HelperClasses;
 
 namespace SpacePro.Controllers.BodiesControllers
 {
     public class BodiesController : Controller
     {
-        ApplicationDbContext db;
+        UnitOfWork unitOfWork;
         public BodiesController()
         {
-            db = new ApplicationDbContext();
+            unitOfWork = new UnitOfWork(new ApplicationDbContext());
         }
         // GET: Bodies
         public ActionResult BodiesTable()
@@ -22,67 +24,25 @@ namespace SpacePro.Controllers.BodiesControllers
         public ActionResult GetBodies()
         {
             List<object> bodies = new List<object>();
-            var moons = db.Moons.Select(m => new
-            {
-                Id = m.MoonId,
-                Name = m.Name,
-                VolValue = m.VolValue,
-                MassValue = m.MassValue,
-                Dimension = m.Dimension,
-                DiscoveredBy = m.DiscoveredBy,
-                DiscoveryDate = m.DiscoveryDate,
-                BodyType= "Moon"
-            });
+            var moons = unitOfWork.Moons.GetAll().TransformListOfMoonsToListOfObjects();
             bodies.AddRange(moons);
-            var planets = db.Planets.Select(p => new
-            {
-                Id = p.PlanetId,
-                Name = p.Name,
-                VolValue = p.VolValue,
-                MassValue = p.MassValue,
-                Dimension = p.Dimension,
-                DiscoveredBy = p.DiscoveredBy,
-                DiscoveryDate = p.DiscoveryDate,
-                BodyType = "Planet"
-            });
+            var planets = unitOfWork.Planets.GetAll().TransformListOfPlanetsToListOfObjects();
             bodies.AddRange(planets);
-            var asteroids = db.Asteroids.Select(a => new
-            {
-                Id = a.AsteroidId,
-                Name = a.Name,
-                VolValue = a.VolValue,
-                MassValue = a.MassValue,
-                Dimension = a.Dimension,
-                DiscoveredBy = a.DiscoveredBy,
-                DiscoveryDate = a.DiscoveryDate,
-                BodyType = "Asteroid"
-            });
+            var asteroids = unitOfWork.Asteroids.GetAll().TransformListOfAsteroidsToListOfObjects();
             bodies.AddRange(asteroids);
-            var comets = db.Comets.Select(c => new
-            {
-                Id = c.CometId,
-                Name = c.Name,
-                VolValue = c.VolValue,
-                MassValue = c.MassValue,
-                Dimension = c.Dimension,
-                DiscoveredBy = c.DiscoveredBy,
-                DiscoveryDate = c.DiscoveryDate,
-                BodyType = "Comet"
-            });
+            var comets = unitOfWork.Comets.GetAll().TransformListOfCometsToListOfObjects();
             bodies.AddRange(comets);
-            var stars = db.Stars.Select(s => new
-            {
-                Id = s.StarId,
-                Name = s.Name,
-                VolValue = s.VolValue,
-                MassValue = s.MassValue,
-                Dimension = s.Dimension,
-                DiscoveredBy = s.DiscoveredBy,
-                DiscoveryDate = s.DiscoveryDate,
-                BodyType = "Star"
-            });
+            var stars = unitOfWork.Stars.GetAll().TransformListOfStarsToListOfObjects();
             bodies.AddRange(stars);
-            return Json(new { data = bodies },JsonRequestBehavior.AllowGet);
+            return Json(new { data = bodies }, JsonRequestBehavior.AllowGet);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ SpacePro/Controllers/HelperClasses/BodiesHelper.cs
@@ -0,0 +1,82 @@
+﻿using Entities.Bodies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpacePro.Controllers.HelperClasses
+{
+    public static class BodiesHelper
+    {
+        public static IEnumerable<object> TransformListOfMoonsToListOfObjects(this IEnumerable<Moon> moons)
+        {
+            return moons.Select(m => new
+            {
+                Id = m.MoonId,
+                Name = m.Name,
+                VolValue = m.VolValue,
+                MassValue = m.MassValue,
+                Dimension = m.Dimension,
+                DiscoveredBy = m.DiscoveredBy,
+                DiscoveryDate = m.DiscoveryDate,
+                BodyType = "Moon"
+            });
+        } 
+        public static IEnumerable<object> TransformListOfAsteroidsToListOfObjects(this IEnumerable<Asteroid> asteroids)
+        {
+            return asteroids.Select(m => new
+            {
+                Id = m.AsteroidId,
+                Name = m.Name,
+                VolValue = m.VolValue,
+                MassValue = m.MassValue,
+                Dimension = m.Dimension,
+                DiscoveredBy = m.DiscoveredBy,
+                DiscoveryDate = m.DiscoveryDate,
+                BodyType = "Asteroid"
+            });
+        } 
+        public static IEnumerable<object> TransformListOfPlanetsToListOfObjects(this IEnumerable<Planet> planets)
+        {
+            return planets.Select(m => new
+            {
+                Id = m.PlanetId,
+                Name = m.Name,
+                VolValue = m.VolValue,
+                MassValue = m.MassValue,
+                Dimension = m.Dimension,
+                DiscoveredBy = m.DiscoveredBy,
+                DiscoveryDate = m.DiscoveryDate,
+                BodyType = "Planet"
+            });
+        }
+        public static IEnumerable<object> TransformListOfCometsToListOfObjects(this IEnumerable<Comet> comets)
+        {
+            return comets.Select(m => new
+            {
+                Id = m.CometId,
+                Name = m.Name,
+                VolValue = m.VolValue,
+                MassValue = m.MassValue,
+                Dimension = m.Dimension,
+                DiscoveredBy = m.DiscoveredBy,
+                DiscoveryDate = m.DiscoveryDate,
+                BodyType = "Comet"
+            });
+        } 
+        public static IEnumerable<object> TransformListOfStarsToListOfObjects(this IEnumerable<Star> stars)
+        {
+            return stars.Select(m => new
+            {
+                Id = m.StarId,
+                Name = m.Name,
+                VolValue = m.VolValue,
+                MassValue = m.MassValue,
+                Dimension = m.Dimension,
+                DiscoveredBy = m.DiscoveredBy,
+                DiscoveryDate = m.DiscoveryDate,
+                BodyType = "Star"
+            });
+        }
+    }
+}
