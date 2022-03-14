ea1ebf624a264059c6d7f396bbc021ea5b439572
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-14 17:12:30 +0200

    Extracted Script from BodiesTable View

---
+++ SpacePro/Scripts/BodiesScripts/PopulatingBodiesDatatable.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $('#bodiesTable').DataTable({
+        ajax: {
+            url: "/Bodies/GetBodies",
+            type: "GET",
+            dataType: "json"
+        },
+        columns: [
+            {
+                data: "BodyType"
+            },
+            {
+                data: "Name"
+            },
+            {
+                data: "VolValue"
+            },
+            {
+                data: "MassValue"
+            },
+            {
+                data: "Dimension"
+            },
+            {
+                data: "DiscoveredBy"
+            },
+            {
+                data: "DiscoveryDate", render: function (date) {
+                    if (date != null) {
+                        let mydate = moment.utc(date);
+                        return mydate.format('DD-MM-YYYY');
+                    }
+                    else {
+                        return date;
+                    }
+                }
+            }
+
+        ]
+    });
+});
