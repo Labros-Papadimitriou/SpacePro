5aa729909690d8b9244654a4a448b601f25efd42
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-19 21:11:46 +0200

    Turned methods into static and the basic two into extension.

--- SpacePro/Controllers/HelperClasses/ApplicationUserHelper.cs
+++ SpacePro/Controllers/HelperClasses/ApplicationUserHelper.cs
@@ -7,12 +7,12 @@ using System.Web;
 
 namespace SpacePro.Controllers.HelperClasses
 {
-    public class ApplicationUserHelper
+    public static class ApplicationUserHelper
     {
         #region SubOfTheMonth
-        public ApplicationUser GetSubOfTheMonth(Persistance_UnitOfWork.IUnitOfWork unitOfWork)
+        public static ApplicationUser GetSubOfTheMonth(this IEnumerable<ApplicationUser> users)
         {
-            var subs = GetSubscribers(unitOfWork);
+            var subs = GetSubscribers(users);
             var count = subs.Count();
             if (IsLastDay(DateTime.Now) && !IsSubCountZero(count))
             {
@@ -23,42 +23,45 @@ namespace SpacePro.Controllers.HelperClasses
             return null;
         }
 
-        public bool IsLastDay(DateTime date) => date.Day == DateTime.DaysInMonth(date.Year, date.Month);
-        public bool IsSubCountZero(int count) => count == 0;
-        public ApplicationUser GetWinner(IEnumerable<ApplicationUser> subs, int randomIndex)
+        public static bool IsLastDay(DateTime date) => date.Day == DateTime.DaysInMonth(date.Year, date.Month);
+        public static bool IsSubCountZero(int count) => count == 0;
+        public static ApplicationUser GetWinner(IEnumerable<ApplicationUser> subs, int randomIndex)
         {
             return subs.Where((x, i) => i == randomIndex).FirstOrDefault();
         }
-        public int GetRandomIndex(int count)
+        public static int GetRandomIndex(int count)
         {
             Random random = new Random();
             return random.Next(count);
         }
         //this requires finding the roles name to string in order to work
-        public IEnumerable<ApplicationUser> GetSubscribers(Persistance_UnitOfWork.IUnitOfWork unitOfWork)
+        public static IEnumerable<ApplicationUser> GetSubscribers(IEnumerable<ApplicationUser> users)
         {
-            return unitOfWork.ApplicationUsers.GetAll().Where(x=>x.Roles.Any(s=>s.Equals("Subscriber")));
+            return users.Where(x=>x.Roles.Any(s=>s.Equals("Subscriber")));
         }
         #endregion
 
         #region AuthorOfTheMonth
-        public ApplicationUser GetAuthorOfTheMonth(Persistance_UnitOfWork.IUnitOfWork unitOfWork)
+        public static ApplicationUser GetAuthorOfTheMonth(this IEnumerable<ApplicationUser> users)
         {
-            var authors = GetAuthors(unitOfWork);
+            var authors = GetAuthors(users);
             if (IsLastDay(DateTime.Now)&&IsSubCountZero(authors.Count()))
             {
                 return GetAuthorWithMostLikes(authors);
             }
             return null;
         }
-        public IEnumerable<ApplicationUser> GetAuthors(Persistance_UnitOfWork.IUnitOfWork unitOfWork)
+        //this requires finding the roles name to string in order to work
+        public static IEnumerable<ApplicationUser> GetAuthors(IEnumerable<ApplicationUser> users)
         {
-            return unitOfWork.ApplicationUsers.GetAll().Where(x => x.Roles.Any(s => s.Equals("Author")));
+            return users.Where(x => x.Roles.Any(s => s.Equals("Author")));
         }
-        public ApplicationUser GetAuthorWithMostLikes(IEnumerable<ApplicationUser> authors)
+        public static ApplicationUser GetAuthorWithMostLikes(IEnumerable<ApplicationUser> authors)
         {
-            return authors.Select(x=>new {auth=x, likes=x.UserPosts.OrderByDescending(p=>p.PostLikes).FirstOrDefault()})
-                .OrderByDescending(x=>x.likes).FirstOrDefault().auth;
+            return authors
+                .Where(x=>x.UserPosts.Count()>0)
+                .Select(x=>new {auth=x, likes=x.UserPosts.OrderByDescending(p=>p.PostLikes).First().PostLikes})
+                .OrderByDescending(x=>x.likes).First().auth;
         }
         #endregion
     }
