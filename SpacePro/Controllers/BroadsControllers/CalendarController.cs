b1bfabae3e757d41f51106d70f039ac70874269e
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-20 17:49:21 +0200

    Fixed 4 name related errors at calendar and added some methods to the controller.

--- SpacePro/Controllers/BroadsControllers/CalendarController.cs
+++ SpacePro/Controllers/BroadsControllers/CalendarController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyDatabase;
+using Persistance_UnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,28 @@ namespace SpacePro.Controllers.BroadsControllers
 {
     public class CalendarController : Controller
     {
+        readonly UnitOfWork unitOfWork;
+        public CalendarController()
+        {
+            unitOfWork = new UnitOfWork(new ApplicationDbContext());
+        }
         public ActionResult ShowCalendar()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult GetEvents()
+        {
+           var events= unitOfWork.Events.GetAll();
+            return Json(new { data = events });
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
