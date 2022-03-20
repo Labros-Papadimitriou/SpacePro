1b4f576de767de3da11b21dc679851976540415d
tsikriteasraf <tsikriteasraf@gmail.com>
2022-03-20 21:50:04 +0200

    exported script from layout to external js file

---
+++ SpacePro/Scripts/LayoutScripts/UserImageSeed.js
@@ -0,0 +1,19 @@
+﻿
+var settings = {
+    "url": "/AppUser/GetUserImage",
+    "method": "GET",
+    "timeout": 0,
+};
+
+$.ajax(settings).done(function (dataImg) {
+    let temp = `<img  src="/Template/sash/assets/images/users/7.jpg" alt="profile-user" class="avatar  profile-user brround cover-image">`;
+
+
+    if (dataImg.data != "") {
+        temp = `<img  src="${dataImg.data.Url}" alt="${dataImg.data.AlternativeText}" class="avatar  profile-user brround cover-image">`;
+    }
+
+    let img = $(temp);
+
+    $("#userImgImg").append(img);
+});
