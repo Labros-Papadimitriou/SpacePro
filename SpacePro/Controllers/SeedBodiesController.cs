6b91e60d98b7994c105d281a4206974396dcc598
Labros Papadimitriou <papadimitriou.lb@gmail.com>
2022-04-02 18:35:42 +0300

    Fixed SeedBodies Controller to work Async

--- SpacePro/Controllers/SeedBodiesController.cs
+++ SpacePro/Controllers/SeedBodiesController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,28 +26,28 @@ namespace SpacePro.Controllers
         }
 
         [HttpPost]
-        public void CreatePlanets(List<Planet> planets)
+        public async Task CreatePlanets(List<Planet> planets)
         {
             foreach (var planet in planets)
             {
                 _unitOfWork.Planets.Add(planet);
             }
-            _unitOfWork.Complete();
+            await _unitOfWork.Complete();
         }
 
         [HttpPost]
-        public async void CreateMoons(List<Moon> apiMoons)
+        public async Task CreateMoons(List<Moon> apiMoons)
         {
             var allPlanets = await _unitOfWork.Planets.GetAll();
             var moons = apiMoons.Where(x => allPlanets.Any(p => x.aroundPlanet.ToLower() == p.Name.ToLower()));
-            moons = (IEnumerable<Moon>)moons.Select(async x => new Moon
+            moons = moons.Select(x => new Moon
             {
                 Name = x.Name,
                 MassValue = x.MassValue,
                 VolValue = x.VolValue,
                 DiscoveredBy = x.DiscoveredBy,
                 DiscoveryDate = x.DiscoveryDate,
-                PlanetId = (await _unitOfWork.Planets.GetAll()).FirstOrDefault(p => p.Name.ToLower() == x.aroundPlanet.ToLower()).PlanetId
+                PlanetId = allPlanets.FirstOrDefault(p => p.Name.ToLower() == x.aroundPlanet.ToLower()).PlanetId
             });
             foreach (var moon in moons)
             {
@@ -56,33 +57,33 @@ namespace SpacePro.Controllers
         }
 
         [HttpPost]
-        public void CreateComets(List<Comet> comets)
+        public async Task CreateComets(List<Comet> comets)
         {
             foreach (var comet in comets)
             {
                 _unitOfWork.Comets.Add(comet);
             }
-            _unitOfWork.Complete();
+            await _unitOfWork.Complete();
         }
 
         [HttpPost]
-        public void CreateAsteroids(List<Asteroid> asteroids)
+        public async Task CreateAsteroids(List<Asteroid> asteroids)
         {
             foreach (var asteroid in asteroids)
             {
                 _unitOfWork.Asteroids.Add(asteroid);
             }
-            _unitOfWork.Complete();
+            await _unitOfWork.Complete();
         }
 
         [HttpPost]
-        public void CreateStars(List<Star> stars)
+        public async Task CreateStars(List<Star> stars)
         {
             foreach (var star in stars)
             {
                 _unitOfWork.Stars.Add(star);
             }
-            _unitOfWork.Complete();
+            await _unitOfWork.Complete();
         }
 
         protected override void Dispose(bool disposing)
